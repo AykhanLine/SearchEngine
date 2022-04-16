95550501e3c92a4e6541ec2ab0b5917ea80c7997
AykhanLine <ayxankerimli2001@mail.ru>
2022-04-16 15:05:24 +0400

    Add project files.

---
+++ SearchEngine/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using SearchEngine.Service;
+
+SearchService serviceSearch = new();
+
+Console.WriteLine("Axtaris sozu daxil edin");
+
+var words = Console.ReadLine();
+
+foreach (var word in serviceSearch.Searching(words))
+{
+    Console.WriteLine(word.Title + " " + word.Description);
+}
---
+++ SearchEngine/Service/SearchService.cs
@@ -0,0 +1,49 @@
+﻿using SearchEngine.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchEngine.Service
+{
+    public class SearchService
+    {
+        List<Search> results = new()
+        {
+            new Search
+            {
+                Id = 1,
+                Title = "KURSLAR.AZ - KURSLAR və Telimler, Online kurslar",
+                Description = "Telimlər, Online kurslar. Bakida hazirliq kurslari 2022",
+                Url = "adswff"
+
+            },
+            new Search
+            {
+                Id = 2,
+                Title = "KURSLAR.AZ - KURSLAR və Telimler, Online kurslar",
+                Description = "Telimlər, Online kurslar. Bakida hazirliq kurslari 2022",
+                Url = "fdggtg"
+
+            },
+            new Search
+            {
+                Id = 3,
+                Title = "KURSLAR.AZ - KURSLAR və Telimler, Online kurslar",
+                Description = "Telimlər, Online kurslar. Bakida hazirliq kurslari 2022",
+                Url = "rgggge"
+
+            }
+        };
+
+        public List<Search> Searching(string Name)
+        {
+            var res = results.Where(x => x.Title.Contains(Name) || x.Description.Contains(Name)).ToList();
+            return results;
+        }
+
+
+
+    }
+}
